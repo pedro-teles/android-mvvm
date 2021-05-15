e18200d6aaeda732a9234f21ebbaba206a1e5a71
Pedro Teles <pedroateles@protonmail.com>
2021-05-15 11:14:18 -0300

    initial commit

---
+++ app/src/main/java/dev/pedroteles/cyberflix/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.pedroteles.cyberflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
